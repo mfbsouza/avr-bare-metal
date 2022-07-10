a89c5ccf4366b369052cbf74b7cb2b713f0a440b
mfbsouza <mfbsouza.it@gmail.com>
2022-07-10 16:04:19 -0300

    cleaning
    Signed-off-by: mfbsouza <mfbsouza.it@gmail.com>

--- arch/atmega328p/config.mk
+++ arch/atmega328p/config.mk
@@ -1,7 +1,13 @@
 # config related to AVR ATmega 328p boards
 
-PREFIX      := avr-
-ARCHCFLAGS  := -mmcu=atmega328p -DF_CPU=16000000UL
-ARCHLDFLAGS := -mmcu=atmega328p -Wl,-Map=$(BUILDDIR)/memory.map
+CFLAGS   += -mmcu=atmega328p -DF_CPU=16000000UL
+CXXFLAGS += -mmcu=atmega328p -DF_CPU=16000000UL
+LDFLAGS  += -mmcu=atmega328p -Wl,-Map=$(BUILDDIR)/memory.map
+
+# linker flags for no avr libc support
+
+#ALLASMSRCS  += ./arch/atmega328p/vectors.s
+#LDSCRIPT    += ./arch/atmega328p/lscript.ld
+#ASMFLAGS    += -Wall -I $(INCDIR) $(ARCHCFLAGS) -MMD -MP
 #ARCHLDFLAGS := -mmcu=atmega328p -nostdlib -nodefaultslibs -nostartfiles -T $(LDSCRIPT) -Wl,-Map=$(BUILDDIR)/memory.map
 
--- arch/attiny85/config.mk
+++ arch/attiny85/config.mk
@@ -1,13 +1,5 @@
 # config related to AVR ATtiny 85 boards
 
-PREFIX      := avr-
-ARCHCFLAGS  := -mmcu=attiny85
-ARCHLDFLAGS := -mmcu=attiny85 -Wl,-Map=$(BUILDDIR)/memory.map
-
-# Shared variable
-
-ALLCFLAGS += \
-	$(ARCHCFLAGS)
-
-ALLLDFLAGS += \
-	$(ARCHLDFLAGS)
+CFLAGS   += -mmcu=attiny85
+CXXFLAGS += -mmcu=attiny85
+LDFLAGS  += -mmcu=attiny85 -Wl,-Map=$(BUILDDIR)/memory.map
